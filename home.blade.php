540ea42631f64d86d4ada12e854bcee75b866d6f
rastgotci230164-ux <rastgo.tci230164@spu.edu.iq>
2026-05-20 16:18:14 +0300

    Add files via upload

---
+++ home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+<div class="text-center">
+    <h1>Welcome to My Shop</h1>
+    <a href="/products" class="btn btn-success mt-3">View Products</a>
+</div>
+@endsection
